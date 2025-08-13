0231744b2e4d515471e768f385d1a109547ef745
José Quintinno <josequintino@hotmail.com.br>
2025-08-13 18:09:58 -0300

    build: Configuração para rodar no ambiente de Produção

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ mkdir -p ../defensiumlog
 
 pkill -f "java -jar $JAR_PATH" || true
 
-nohup java -jar target/defensium-service.jar > ../defensiumlog/application.log 2>&1 &
+nohup java -jar target/defensium-service.jar   > ../defensiumlog/application.log 2>&1 &
 
 sleep 5
 
