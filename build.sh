5a39c60b0fe8f6eb86ff4ce2e19c78657501b37b
José Quintinno <josequintino@hotmail.com.br>
2025-08-13 18:12:55 -0300

    build: Configuração para rodar no ambiente de Produção

--- build.sh
+++ build.sh
@@ -10,6 +10,4 @@ mvn clean install -DskipTests
 
 nohup java -jar target/defensium-service.jar > ../defensiumlog/application.log 2>&1 &
 
-sleep 5
-
-tail -n 500 -f ../defensiumlog/application.log
+# tail -n 500 -f ../defensiumlog/application.log
